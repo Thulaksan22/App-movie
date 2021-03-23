54a5fa9ace6e18a0602249045f9004229e2df030
Thulaksan22 <54886806+Thulaksan22@users.noreply.github.com>
2021-03-23 17:25:59 +0100

    preparation finis

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from "react"; 
 import './App.css';
+
+
+import Movies from "./Components/Movies";
+
+
 
 function App() {
   return (
     <div className="App">
-
+      <Movies/>
     </div>
   );
 }
